720f94d0f8b0d8a2d3d02b9c3184894c56240b6b
Bonjour LI <33248848+bonjour-py@users.noreply.github.com>
2025-12-25 15:03:04 +0800

    feat: Enhance Incus preseed configuration

--- nixos/incus/flake.nix
+++ nixos/incus/flake.nix
@@ -6,7 +6,71 @@
           enable = true;
           package = pkgs.incus;
           ui.enable = true;
-          preseed.config."core.https_address" = "[::]:443";
+          preseed = {
+            config."core.https_address" = "[::]:443";
+            storage_pools = [
+              {
+                config = {
+                  source = "/var/lib/incus/storage-pools/default";
+                  "volatile.initial_source" = "/var/lib/incus/storage-pools/default";
+                };
+                description = "";
+                name = "default";
+                driver = "btrfs";
+              }
+            ];
+            profiles = [
+              {
+                config = { };
+                description = "Default Incus profile";
+                devices = { };
+                name = "default";
+                project = "";
+              }
+            ];
+            projects = [
+              {
+                config = {
+                  "features.images" = "true";
+                  "features.networks" = "true";
+                  "features.networks.zones" = "true";
+                  "features.profiles" = "true";
+                  "features.storage.buckets" = "true";
+                  "features.storage.volumes" = "true";
+                };
+                description = "Default Incus project";
+                name = "default";
+              }
+            ];
+            certificates = [
+              {
+                name = "bonjour";
+                type = "client";
+                restricted = false;
+                projects = [ ];
+                certificate = ''
+                  -----BEGIN CERTIFICATE-----
+                  MIICuzCCAkKgAwIBAgIUNkh+ap//1gQJCZmQGA4FeBg08acwCgYIKoZIzj0EAwMw
+                  gZMxCzAJBgNVBAYTAkNOMRAwDgYDVQQIDAdCZWlqaW5nMRAwDgYDVQQHDAdCZWlq
+                  aW5nMRMwEQYDVQQKDApCb25qb3VyIExJMRIwEAYDVQQLDAlCb25qb3VyTEkxEjAQ
+                  BgNVBAMMCUJvbmpvdXJMSTEjMCEGCSqGSIb3DQEJARYUYm9uam91ckBib25qb3Vy
+                  LnpvbmUwIBcNMjQxMTEzMDM1MzMwWhgPMjEyNDEwMjAwMzUzMzBaMIGTMQswCQYD
+                  VQQGEwJDTjEQMA4GA1UECAwHQmVpamluZzEQMA4GA1UEBwwHQmVpamluZzETMBEG
+                  A1UECgwKQm9uam91ciBMSTESMBAGA1UECwwJQm9uam91ckxJMRIwEAYDVQQDDAlC
+                  b25qb3VyTEkxIzAhBgkqhkiG9w0BCQEWFGJvbmpvdXJAYm9uam91ci56b25lMHYw
+                  EAYHKoZIzj0CAQYFK4EEACIDYgAE/q65C5nSYaiOm6095pQGnOEAk7P2mW0FkUyJ
+                  zc5SWtvC+UOiqQiBTAcsiiVfcgYUCE2iYHrstn5srzHPfDQoLLrjpoklT6wWBIsp
+                  EJhspMTqZXhaetfziVyp8XoyfIkCo1MwUTAdBgNVHQ4EFgQUri21cSjdebXomtH4
+                  /keddL5AvH4wHwYDVR0jBBgwFoAUri21cSjdebXomtH4/keddL5AvH4wDwYDVR0T
+                  AQH/BAUwAwEB/zAKBggqhkjOPQQDAwNnADBkAjBxDx4/PybKsUTBwp05R9x1fTGw
+                  zw13DlbWiPCJ8zVD36gg9bUw4NEXZahvnlyZ+WsCMGjwOUIauvigu9L4ratzY+ub
+                  teWflOpPwmTN82ioIqlakbrFCQy4GJYHGHGxYWO0XA==
+                  -----END CERTIFICATE-----
+                '';
+                description = "Bonjour LI";
+              }
+            ];
+          };
         };
         security.apparmor.enable = true;
         networking.firewall.allowedTCPPorts = [ 80 443 ];
@@ -14,9 +78,7 @@
       server = {
         imports = [self.nixosModules.default];
         virtualisation.incus.preseed = {
-          config = {
-            "user.ui.title" = "server.bonjour";
-          };
+          config."user.ui.title" = "server.bonjour";
           networks = [
             {
               config = {
@@ -41,26 +103,7 @@
               project = "default";
             }
           ];
-          storage_pools = [
-            {
-              config = {
-                source = "/var/lib/incus/storage-pools/default";
-                "volatile.initial_source" = "/var/lib/incus/storage-pools/default";
-              };
-              description = "";
-              name = "default";
-              driver = "btrfs";
-            }
-          ];
-          storage_volumes = [ ];
           profiles = [
-            {
-              config = { };
-              description = "Default Incus profile";
-              devices = { };
-              name = "default";
-              project = "";
-            }
             {
               config = {
                 "cloud-init.vendor-data" = ''
@@ -134,18 +177,6 @@
             {
               config = {
                 "features.images" = "true";
-                "features.networks" = "true";
-                "features.networks.zones" = "true";
-                "features.profiles" = "true";
-                "features.storage.buckets" = "true";
-                "features.storage.volumes" = "true";
-              };
-              description = "Default Incus project";
-              name = "default";
-            }
-            {
-              config = {
-                "features.images" = "true";
                 "features.profiles" = "true";
                 "features.storage.buckets" = "true";
                 "features.storage.volumes" = "true";
@@ -155,35 +186,6 @@
               name = "work";
             }
           ];
-          certificates = [
-            {
-              name = "bonjour";
-              type = "client";
-              restricted = false;
-              projects = [ ];
-              certificate = ''
-                -----BEGIN CERTIFICATE-----
-                MIICuzCCAkKgAwIBAgIUNkh+ap//1gQJCZmQGA4FeBg08acwCgYIKoZIzj0EAwMw
-                gZMxCzAJBgNVBAYTAkNOMRAwDgYDVQQIDAdCZWlqaW5nMRAwDgYDVQQHDAdCZWlq
-                aW5nMRMwEQYDVQQKDApCb25qb3VyIExJMRIwEAYDVQQLDAlCb25qb3VyTEkxEjAQ
-                BgNVBAMMCUJvbmpvdXJMSTEjMCEGCSqGSIb3DQEJARYUYm9uam91ckBib25qb3Vy
-                LnpvbmUwIBcNMjQxMTEzMDM1MzMwWhgPMjEyNDEwMjAwMzUzMzBaMIGTMQswCQYD
-                VQQGEwJDTjEQMA4GA1UECAwHQmVpamluZzEQMA4GA1UEBwwHQmVpamluZzETMBEG
-                A1UECgwKQm9uam91ciBMSTESMBAGA1UECwwJQm9uam91ckxJMRIwEAYDVQQDDAlC
-                b25qb3VyTEkxIzAhBgkqhkiG9w0BCQEWFGJvbmpvdXJAYm9uam91ci56b25lMHYw
-                EAYHKoZIzj0CAQYFK4EEACIDYgAE/q65C5nSYaiOm6095pQGnOEAk7P2mW0FkUyJ
-                zc5SWtvC+UOiqQiBTAcsiiVfcgYUCE2iYHrstn5srzHPfDQoLLrjpoklT6wWBIsp
-                EJhspMTqZXhaetfziVyp8XoyfIkCo1MwUTAdBgNVHQ4EFgQUri21cSjdebXomtH4
-                /keddL5AvH4wHwYDVR0jBBgwFoAUri21cSjdebXomtH4/keddL5AvH4wDwYDVR0T
-                AQH/BAUwAwEB/zAKBggqhkjOPQQDAwNnADBkAjBxDx4/PybKsUTBwp05R9x1fTGw
-                zw13DlbWiPCJ8zVD36gg9bUw4NEXZahvnlyZ+WsCMGjwOUIauvigu9L4ratzY+ub
-                teWflOpPwmTN82ioIqlakbrFCQy4GJYHGHGxYWO0XA==
-                -----END CERTIFICATE-----
-              '';
-              description = "Bonjour LI";
-            }
-          ];
-          cluster_groups = [ ];
         };
         fileSystems."/var/lib/incus" = {
           device = "UUID=297e0847-bab9-4d30-90e0-6163e404ebcf";
