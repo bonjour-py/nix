335ac5c5cf69a7fff28d98e63293d8dcb2a32f51
bonjour-py <33248848+bonjour-py@users.noreply.github.com>
2026-03-19 11:02:26 +0800

    update

--- nixos/gateway/hardware.nix
+++ nixos/gateway/hardware.nix
@@ -2,14 +2,14 @@
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_blk" ];
   fileSystems = {
     "/boot" = {
-      device = "/dev/vda1";
+      device = "UUID=27C5-280F";
       fsType = "vfat";
       options = ["fmask=0077" "dmask=0077"];
     };
     "/" = {
-      device = "/dev/vda2";
+      device = "UUID=8706d89b-ddfb-4b89-8aac-394e2373bc05";
       fsType = "ext4";
     };
   };
-  swapDevices = [{device = "/dev/vda3";}];
+  swapDevices = [{device = "UUID=26c12c19-1373-4697-b6a6-9428e7039509";}];
 }
--- nixos/laptop/default.nix
+++ nixos/laptop/default.nix
@@ -4,23 +4,6 @@
     ../default/nix.nix
     ./hardware.nix
     ./hostname.nix
+    ./desktop.nix
   ];
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  xdg.portal.config.common.default = "*";
-  services.flatpak.enable = true;
-  services.flatpak.remotes = [
-    {
-    name = "flathub";
-    location = "https://mirrors.cernet.edu.cn/flathub";
-    }
-  ];
-  services.flatpak.packages = [
-    "com.dingtalk.DingTalk"
-  ];
-  home-manager.users.bonjour = {pkgs, ...}:{
-    home = {
-      stateVersion = "25.11";
-    };
-  };
 }
