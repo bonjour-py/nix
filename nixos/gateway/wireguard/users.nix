44f243eadad2fb1285aac28023e09c624dc0bd14
Bonjour LI <33248848+bonjour-py@users.noreply.github.com>
2026-03-22 11:26:22 +0000

    back

--- nixos/gateway/wireguard/users.nix
+++ nixos/gateway/wireguard/users.nix
@@ -15,10 +15,12 @@
     {
       publicKey = "jONbtJPcVxavD+LsTb0Ab1iNzDco5dvGA62GxoTW/Wc=";
       presharedKeyFile = config.age.secrets."wireguard/gateway-yunoki.preshared".path;
+      allowedIPs = ["192.168.59.241/32"];
     }
     {
       publicKey = "1W+Uz05wl5DK8SbMpYg0ujRMRL7UAxXf7LGlA4D45XY=";
       presharedKeyFile = config.age.secrets."wireguard/gateway-kanoi.preshared".path;
+      allowedIPs = ["192.168.59.242/32"];
     }
   ];
 }
