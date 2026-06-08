1e9bae6f266fca14d87c9fc5e926de6c2e03b651
bonjour <33248848+bonjour-py@users.noreply.github.com>
2026-06-08 18:22:35 +0800

    update

--- nixos/gateway/hardware.nix
+++ nixos/gateway/hardware.nix
@@ -2,14 +2,14 @@
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_blk" ];
   fileSystems = {
     "/boot" = {
-      device = "/dev/disk/by-label/boot";
+      device = "UUID=3D8C-2E2A";
       fsType = "vfat";
       options = ["fmask=0077" "dmask=0077"];
     };
     "/" = {
-      device = "/dev/disk/by-label/root";
+      device = "UUID=d28ca22c-57ff-4343-88d3-fee40011d251";
       fsType = "ext4";
     };
   };
-  swapDevices = [{device = "/dev/disk/by-label/swap";}];
+  swapDevices = [{device = "/dev/disk/by-uuid/afce08be-b0a5-4bc8-b8ca-3ff0df623cda";}];
 }
