82348edae1ed932c778c08468fb9a64b7cbd83dc
bonjour <33248848+bonjour-py@users.noreply.github.com>
2026-09-11 16:19:37 +0800

    update

--- packages/work/wxwork.nix
+++ packages/work/wxwork.nix
@@ -2,5 +2,5 @@
   name = "work.wxwork";
   desktopName = "企业微信";
   icon = ./icons/windows.svg;
-  exec = ''${pkgs.remmina}/bin/remmina -c "rdp://bonjour:zdU76+p0wBg=@192.168.110.92" --set-option "sound=local" --set-option "scale=2" --set-option "multitransport=1" --enable-fullscreen'';
+  exec = ''${pkgs.remmina}/bin/remmina -c "rdp://bonjour:zdU76+p0wBg=@192.168.110.92" --set-option "sound=local" --set-option "scale=2" --set-option "multitransport=1" --set-option "drive=/home/bonjour/download" --enable-fullscreen'';
 }
